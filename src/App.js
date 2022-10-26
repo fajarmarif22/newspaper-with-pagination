bceeb9be79f17857caaea5302f1c16937bb90cd0
fmaulanaarif@gmail.com <fmaulanaarif@gmail.com>
2022-10-26 19:03:38 +0700

    apps pagination

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import SearchForm from './searchForm'
+import Stories from './stories'
+import Buttons from './button'
+function App() {
+  return (
+    <>
+      <SearchForm />
+      <Buttons />
+      <Stories />
+    </>
+  )
+}
+
+export default App
